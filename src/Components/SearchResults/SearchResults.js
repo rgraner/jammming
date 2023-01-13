d325061ae570830b807f0edb6e7f9457892e4489
rgraner <ricardograner@gmail.com>
2023-01-13 10:25:31 +0000

    First commit

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { TrackList } from '../TrackList/TrackList'
+
+class SearchResults extends React.Components {
+    render() {
+        return (
+            <div className="SearchResults">
+                <h2>Results</h2>
+                {/* Add a TrackList component */}
+            </div>
+        )
+    }
+}
