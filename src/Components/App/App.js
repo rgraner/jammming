38d824b3ac672e9b89561b7b6a4bbcf86bd8f14b
rgraner <ricardograner@gmail.com>
2023-01-15 21:30:47 +0000

    Implement Spotify search request

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { SearchBar } from '../SearchBar/SearchBar'
 import { SearchResults } from '../SearchResults/SearchResults'
 import { Playlist } from '../Playlist/Playlist'
+import { Spotify } from '../../util/Spotify';
 import React from 'react';
 
 
@@ -54,7 +55,9 @@ class App extends React.Component {
   }
 
   search(term) {
-    console.log(term);
+    Spotify.search(term).then(searchResults => {
+      this.setState({searchResults: searchResults})
+    })
   }
 
   render() {
